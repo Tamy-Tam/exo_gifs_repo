46de8f8cb69288bc44a44fa5b81a3f0294727854
Tamara Robson <tamara.jade.robson@gmail.com>
2021-04-08 22:25:32 +0200

    Added a env file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GIPHY_API_KEYFROM from '../.env';
+
 import '../assets/stylesheets/application.scss';
 
 const Hello = ({ name }) => {
