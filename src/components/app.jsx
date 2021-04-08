af3950097978c85bdf313d93eb670cad1433c877
Tamara Robson <tamara.jade.robson@gmail.com>
2021-04-08 22:53:45 +0200

    Created components files

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import SearchBar from 'components/search_bar';
+import Gif from 'components/gif';
+import GifsList from 'components/gifs_list';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render()
+};
+
+export default App;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render()
+};
+
+export default SearchBar;
