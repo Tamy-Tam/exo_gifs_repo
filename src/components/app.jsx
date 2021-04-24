85c7a323f4727f160dbf9fd1ef896bd37d677dab
Tamara Robson <tamara.jade.robson@gmail.com>
2021-04-24 22:09:41 +0200

    Added the gif_list & gifs

---
+++ src/components/app.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  render() {
+    const gifs = [
+      { id: 'xT9IgDEI1iZyb2wqo8' },
+      { id: 'vKTkbOYdIPYmQ' },
+      { id: '12fegBdilUKCRy' }
+    ];
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id="xT9IgDEI1iZyb2wqo8" />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
